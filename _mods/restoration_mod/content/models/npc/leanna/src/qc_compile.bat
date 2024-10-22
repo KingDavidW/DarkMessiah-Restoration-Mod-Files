ca8bc5d8d3cfa4b3219776a4de200cd65ee213d8
kingdavidw <davidwiltos@gmail.com>
2024-10-22 01:23:48 -0400

    QC Removal
    Removed source files!

--- _mods/restoration_mod/content/models/npc/leanna/src/qc_compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Steam\steamapps\common\Dark Messiah Might and Magic Single Player\bin\studiomdl" Leanna_cinematic.qc
-pause
--- _mods/restoration_mod/content/models/npc/menelag/src/qc_compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Steam\steamapps\common\Dark Messiah Might and Magic Single Player\bin\studiomdl" menelag_cinematic_ext.qc
-pause
