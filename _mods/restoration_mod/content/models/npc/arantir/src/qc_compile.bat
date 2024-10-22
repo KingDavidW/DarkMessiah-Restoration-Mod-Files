c38cbc3a13dafc8c5563ecc1a760c52c60a65c65
kingdavidw <davidwiltos@gmail.com>
2024-10-22 01:03:04 -0400

    Initial Commit
    All the good stuff

---
+++ _mods/restoration_mod/content/models/npc/arantir/src/qc_compile.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Steam\steamapps\common\Dark Messiah Might and Magic Single Player\bin\studiomdl" EXPORT_arantir_cinematic_ext.qc
+pause
---
+++ _mods/restoration_mod/content/models/npc/phenrig/src/qc_compile.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Steam\steamapps\common\Dark Messiah Might and Magic Single Player\bin\studiomdl" phenrig_cinematic_ext.qc
+pause
